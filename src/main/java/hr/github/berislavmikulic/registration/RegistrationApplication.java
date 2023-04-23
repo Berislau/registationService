76768f8ed3d9607dba00ca8d3e74e738ae920fbe
Berislav Mikulić <github.bm@gmail.com>
2023-04-23 22:28:05 +0200

    Update pom
    - few Pom updates regarding project
    - added spring boot application file
    - removed default main class file

---
+++ src/main/java/hr/github/berislavmikulic/registration/RegistrationApplication.java
@@ -0,0 +1,11 @@
+package hr.github.berislavmikulic.registration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationApplication {
+
+    public static void main(String[] args){
+        SpringBootApplication.run(RegistrationApplication.class, args);
+    }
+}

2cea765d5ccad3089547d0d23efc0e0d1b13aa89
Berislav Mikulić <github.bm@gmail.com>
2023-04-26 00:00:54 +0200

    1.0-SNAPSHOT | Add validator and User class
    - added spring boot validation dependency
    	- might need to move it to DAO class
    	  later
    - added User class

---
+++ src/main/java/hr/github/berislavmikulic/registration/model/User.java
@@ -0,0 +1,33 @@
+package hr.github.berislavmikulic.registration.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class User {
+    @NonNull
+    private Long id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NonNull
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String userName;
+    @NonNull
+    @NotBlank
+    @Size(min=8, max = 100)
+    private String password;
+    @NotBlank
+    @Email
+    private String email;
+    private LocalDate dateOfBirth;
+    private String address;
+}
